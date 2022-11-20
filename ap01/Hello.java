ea16f46e5c401ccb3ad7959d21f21fa31836d2fb
JenniferLynn99 <112904272+JenniferLynn99@users.noreply.github.com>
2022-11-20 09:51:39 -0800

    none made

---
+++ ap01/Hello.java
@@ -0,0 +1,8 @@
+public class Hello {
+    
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); //my first line
+        System.out.println("How are you?"); //my second line
+    }
+}
+    
